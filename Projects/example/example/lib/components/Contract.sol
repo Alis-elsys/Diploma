2eab46cb4906687fafc03c0dca1ddb0caeca221b
Alis <aleksandra.p.stoycheva.2019@elsys-bg.org>
2024-01-31 00:06:58 +0200

    full smart contract

--- Projects/example/example/lib/components/Contract.sol
+++ Projects/example/example/lib/components/Contract.sol
@@ -1,62 +1,112 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.7;
 
-contract NFT {
-    address private owner;
-    uint private _tokenId = 0;
-    mapping (uint => string) private _tokenURIs;
-    string private name;
-
-/*    struct NFT {
-        address owner;
-        string name;
-        uint _tokenId;
-        string _tokenURI;
-    }
-    NFT[] private NFTs;
-*/
-    constructor(string memory _name, string memory _tokenURI) {
-        owner = msg.sender;
-        name = _name;
-        _tokenId ++;
-        _tokenURIs[_tId] = _tokenURI;
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner can call this function.");
-        _;
-    }
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract Shop is Ownable, ERC721URIStorage {
     
-    function setName(string memory x) public {
-        name = x;
+    using Counters for Counters.Counter;
+    Counters.Counter private NFTsCounter;
+    Counters.Counter private NFTsIds; 
+    
+    address private Glob_owner;
+
+    struct NFT{
+        uint256 id;
+        string name;
+        address payable NFTowner;
+        string imageUrl; 
+        uint256 price;
     }
 
-    function getName() public view returns (string memory){
-        return name;
-    }
-    function createTokenURI(string memory _tokenURI) public
-    returns(uint, string memory){
-        _tokenURIs[_tokenId] = _tokenURI;
-        _tokenId ++;
-        return(_tokenId, _tokenURI);
+    struct User{
+        address UserAddress;
+        uint256 buyedNFTs;
+        uint256 SoldNFTs; 
+        uint256[] createdNFTs;
+        NFT[] myNFTs;
     }
 
-    function getTokenURI(uint _tId) public view returns (string memory){
-        string memory _currentURI = _tokenURIs[_tId];
-        return _currentURI;
+    mapping(uint256 => NFT) public allNFTs;
+    mapping(address => NFT[]) public  myNFTs;
+    mapping(address => mapping(uint256 => uint256)) public userBoughtCounts;
+    mapping(address => mapping(uint256 => uint256)) public userSoldCounts;
+
+
+    event TokenMinted(uint256 indexed tokenId, address indexed owner, string name, string imageUrl, uint price);
+    event TokenBought(uint256 indexed tokenId, address indexed owner, string name, string imageUrl, uint price);
+
+    constructor(address initialOwner) ERC721("Shop", "SHOP") Ownable(initialOwner) {
+        Glob_owner = initialOwner;
     }
 
-    function getAllTokenURIs() public view returns (string[] memory){
-        string[] memory uris = new string[](_tokenId);
-        for(uint i = 0; i < _tokenId; i++){
-            uris[i] = _tokenURIs[i];
+    function mint(string memory name, string memory imageUrl, uint price) external onlyOwner returns(uint256){
+        
+        uint256 tokenId = NFTsIds.current();
+
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, imageUrl);
+
+        NFT memory newNFT = NFT({
+            id: tokenId,
+            name: name,
+            NFTowner: payable(msg.sender),
+            imageUrl: imageUrl,
+            price: price
+        });        
+        
+        allNFTs[tokenId] = newNFT;
+        myNFTs[msg.sender].push(newNFT);
+
+        
+        emit TokenMinted(tokenId, payable(msg.sender), name, imageUrl, price);
+
+        NFTsCounter.increment();
+        NFTsIds.increment();
+       
+        return tokenId;
+    }
+
+    function buyNFT(uint256 tokenId) external payable{
+        require(msg.value >= allNFTs[tokenId].price, "Not enough ether");
+        require(allNFTs[tokenId].NFTowner != msg.sender, "You are the owner of this NFT");
+
+        payable(allNFTs[tokenId].NFTowner).transfer(msg.value);
+        userBoughtCounts[msg.sender][tokenId]++;
+        userSoldCounts[allNFTs[tokenId].NFTowner][tokenId]++;
+        myNFTs[msg.sender].push(allNFTs[tokenId]);
+        /// myNFTs[msg.sender] = allNFTs[tokenId];
+
+        emit TokenBought(tokenId, payable(msg.sender), allNFTs[tokenId].name, allNFTs[tokenId].imageUrl, allNFTs[tokenId].price);
+    }
+
+    function deleteNFT(uint256 tokenId) external onlyOwner{
+        delete allNFTs[tokenId];
+        myNFTs[msg.sender].pop(); 
+        _burn(tokenId);
+        NFTsCounter.decrement();
+    }
+
+    function getNFT(uint256 tokenId) public view returns(NFT memory){
+        return allNFTs[tokenId];
+    }
+
+    function getAllNFTs() public view returns(NFT[] memory){
+        NFT[] memory allNFTsArray = new NFT[](NFTsCounter.current());
+        for(uint256 i = 0; i < NFTsCounter.current(); i++){
+            allNFTsArray[i] = allNFTs[i];
         }
-        return  uris;
+        return allNFTsArray;
     }
 
-    function addNFT(string memory _name, string memory _tokenURI, uint memory _tokenId) public {
-        NFTs.push(NFT(msg.sender, _name, _tokenId, _tokenURI));
+    function getMyNFTs() public view returns(NFT[] memory){
+        return myNFTs[msg.sender];
     }
 
-    function delete
+    function getLenght() public view returns (uint){
+        return myNFTs[msg.sender].length;
+    }
+
 }
